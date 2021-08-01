122c8c69e9c54a87c05f5d9c4ec2fff7bbd1872b
yarinvk <yarin@fireblocks.com>
2021-08-01 15:35:54 +0300

    tom

--- src/rate-limiter.ts
+++ src/rate-limiter.ts
@@ -1,18 +1,50 @@
 const limitTracker: Record<string, Record<string, {timestamps: number[], index: number, errorCounter: number}>> = {};
 const timeRangeInMilliSeconds = 1000;
 const rateLimit = 5;
+// Todo: set dynamically according to rateLimit
+const emptyTimestamps = [0, 0, 0, 0, 0];
 
 export const canPerformRequest = (userId: string, route: string, timestamp: number): boolean => {
-    updateTracker(userId, route, timestamp);
+    const routeData = getUserRouteData(userId, route);
+    if (isRequestRateValid(routeData, timestamp)) {
+        updateTracker(routeData, timestamp);
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // TODO: Verify keys exist for both user and route
-const isRequestRateValid = (userId: string, route: string, timestamp: number): boolean => {
-    const userData = limitTracker[userId];
-    const routeData = userData[route]
-};
+const isRequestRateValid = (routeData, timestamp: number): boolean =>
+     Math.abs(timestamp - routeData.timestamps[routeData.index]) > timeRangeInMilliSeconds;
+;
 
 // @ts-ignore
-const updateTracker = (userId: string, route: string, timestamp: number) => {
+const updateTracker = (routeData, timestamp: number) => {
+    routeData.timestamps[routeData.index] = timestamp;
+    routeData.index++;
+    trySendErrorLogs(routeData, userId, route);
+    routeData.errorCounter = 0;
+};
 
+const getUserRouteData = (userId: string, route: string): {timestamps: number[], index: number, errorCounter: number} =>{
+    let userData = limitTracker[userId];
+    if (!userData) {
+        userData = {}
+        limitTracker[userId] = userData;
+    }
+    let routeData = userData[route];
+    if (!routeData) {
+        routeData = {timestamps: emptyTimestamps, index: 0, errorCounter: 0};
+        userData[route] = routeData;
+    }
+
+    return routeData;
+};
+
+const trySendErrorLogs = (routeData, userId: string, route: string) => {
+    if (routeData.errorCounter > 0) {
+        console.log(`user ${userId} sent ${routeData.errorCounter} request outside of rate`);
+        routeData.errorCounter = 0;
+    }
 };
